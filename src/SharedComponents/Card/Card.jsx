fed2c49df4deeda529b9134c52adf7e67216c71a
Yusuf Ameri <yusuf.americianaki@gmail.com>
2019-01-13 22:39:42 -0500

    Create StartGameScreen Presentational/functional components

--- src/SharedComponents/Card/Card.jsx
+++ src/SharedComponents/Card/Card.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import "./Card.css"
 
 function Card(props) {
-  let cardType = "card "+ props.cardType;
   if(props.cardType === "Q") {
     props.text.replace("_", "_____________");
   }
 
   if(props.cardType === "Title") {
     return (
-      <div className={cardType}>
+      <div className={`card Title`}>
         <h3>Cards</h3>
         <h3>Against</h3>
         <h3>Humanity</h3>
@@ -17,11 +16,28 @@ function Card(props) {
     );
   }
 
-  return (
-    <div className={cardType}>
-      <p>{props.text}</p>
-    </div>
-  );
+  else if(props.cardType === "Link") {
+    return (
+      <div className={`card Title Link`}>
+        <h3>Invite</h3>
+        <h3>Friends</h3>
+        <h3>with Link</h3>
+        <h6 className="link">
+          <a href={`cardiparty.com/${props.link}`}>
+            {`cardiparty.com/${props.link}`}
+          </a>
+        </h6>
+      </div>
+    )
+  }
+
+  else {
+    return (
+      <div className={"Q"}>
+        <p>{props.text}</p>
+      </div>
+    );
+  }
 }
 
 export default Card;

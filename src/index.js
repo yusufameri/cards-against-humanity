71f23c724833b39a7ad59ed8776f8921b7e7b176
Yusuf Ameri <yusuf.americianaki@gmail.com>
2019-01-15 23:54:47 -0500

    remove grid-row in HomeScreen

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import './normalize.css'
 // import PlayerSelectionScreen from './Screens/PlayerSelectionScreen/PlayerSelectionScreen'
 // ReactDOM.render(<PlayerSelectionScreen />, document.getElementById('root'));
 
-// import HomeScreen from "./Screens/HomeScreen/HomeScreen"
-// ReactDOM.render(<HomeScreen />, document.getElementById('root'));
+import HomeScreen from "./Screens/HomeScreen/HomeScreen"
+ReactDOM.render(<HomeScreen />, document.getElementById('root'));
 
 // import JoinPartyScreen from "./Screens/JoinPartyScreen/JoinPartyScreen"
 // ReactDOM.render(<JoinPartyScreen />, document.getElementById('root'));
 
-import StartGameScreen from "./Screens/StartGameScreen/StartGameScreen"
-ReactDOM.render(<StartGameScreen />, document.getElementById('root'));
+// import StartGameScreen from "./Screens/StartGameScreen/StartGameScreen"
+// ReactDOM.render(<StartGameScreen />, document.getElementById('root'));
 
 

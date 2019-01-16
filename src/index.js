47e86192336c5cc65101110875a1657ab099c33b
Yusuf Ameri <yusuf.americianaki@gmail.com>
2019-01-16 17:43:52 -0500

    remove unneccessary css in PlayerSelectionScreen

--- src/index.js
+++ src/index.js
@@ -5,22 +5,24 @@ import './normalize.css'
 
 // Done
 
-// import PlayerSelectionScreen from './Screens/PlayerSelectionScreen/PlayerSelectionScreen'
-// ReactDOM.render(<PlayerSelectionScreen />, document.getElementById('root'));
-
 // import HomeScreen from "./Screens/HomeScreen/HomeScreen"
 // ReactDOM.render(<HomeScreen />, document.getElementById('root'));
 
 // import JoinPartyScreen from "./Screens/JoinPartyScreen/JoinPartyScreen"
 // ReactDOM.render(<JoinPartyScreen />, document.getElementById('root'));
 
-
-// TODO
-// StartGameScreen (need to)
-import StartGameScreen from "./Screens/StartGameScreen/StartGameScreen"
-ReactDOM.render(<StartGameScreen />, document.getElementById('root'));
-
 // import StartGameScreen from "./Screens/StartGameScreen/StartGameScreen"
 // ReactDOM.render(<StartGameScreen />, document.getElementById('root'));
+
+import PlayerSelectionScreen from './Screens/PlayerSelectionScreen/PlayerSelectionScreen'
+ReactDOM.render(<PlayerSelectionScreen />, document.getElementById('root'));
+
+// TODO
+// import PlayerWaitingScreen from "./Screens/PlayerWaitingScreen/PlayerWaitingScreen"
+// ReactDOM.render(<PlayerWaitingScreen />, document.getElementById('root'));
+
+// import CzarWaitingScreen from "./Screens/CzarWaitingScreen/CzarWaitingScreen"
+// ReactDOM.render(<CzarWaitingScreen />, document.getElementById('root'));
+
 
 

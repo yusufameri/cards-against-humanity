71c665a379511e456b5ce5a50a27dc5279d566ac
Yusuf Ameri <yusuf.americianaki@gmail.com>
2019-01-31 07:25:22 -0800

    judgeSelectCard and endRound working

--- models/Game.js
+++ models/Game.js
@@ -28,7 +28,7 @@ class Game {
       this.players[sessionID] = {
         name,
         pID: _.size(this.players),
-        roundsWon: 0,
+        roundsWon: [],
         cards: this.ACardDeck.splice(0, 3),
         roundState: "lobby"
       };
@@ -58,8 +58,8 @@ class Game {
 
       // O(k), k is constant size number of players
       let round = {
-        roundNum: this.rounds.length + 1,
         active: true,
+        roundNum: this.rounds.length + 1,
         roundState: "players-selecting", // judge-selecting | viewing-winner
         roundStartTime: new Date(),
         roundEndTime: null,
@@ -87,6 +87,7 @@ class Game {
 
   // O(1)
   // Return the round state for the player with given sessionID
+  // TODO: This is broken! Needs refactoring!
   getPlayerRoundState(sessionID) {
     let player = this.getPlayer(sessionID) // O(1)
     if (player == null) return null; // O(1)
@@ -116,7 +117,7 @@ class Game {
         playerRoundState = 'player-waiting'
       }
       else {
-        playerRoundState = 'judge-selecting'
+        playerRoundState = 'judge-selecting' // FIX: this should be accurate. Does not work when roundState is 'viewing-winner'
       }
     }
     else {
@@ -124,14 +125,16 @@ class Game {
     }
     // TODO: return react state object based on player and current round state
     return {
+      roundNum: latestRound.roundNum,
       playerName: player.name,
-      roundRole: 'player',
       roundState: playerRoundState,
       roundJudge: latestRound.roundJudge.name,
-      roundNum: latestRound.roundNum,
+      roundRole: 'player',
       QCard: latestRound.QCard,
       cards: player.cards,
-      otherPlayerCards: latestRound.cardsPlayed
+      cardsPlayed: latestRound.cardsPlayed,
+      winningCard: latestRound.winningCard,
+      winner: latestRound.winner
     }
   }
 
@@ -171,11 +174,48 @@ class Game {
     // cardID choosen is among those in the round.cardsPlayed
 
     // AFTER a judge selects a card, do the following
-    // set round.active = false
     // set round.roundState = viewing-winner
-    // set round.winnerCard
+    let latestRound = this.getLatestRound();
+    if (this.isRoundJudge(sessionID, latestRound)) {
+      let winningCard = _.find(latestRound.cardsPlayed, card => card.id === cardID);
+      if (winningCard) {
+        latestRound.roundState = 'viewing-winner';
+        latestRound.winningCard = winningCard;
+        latestRound.winner = winningCard.owner.name;
+        latestRound.roundEndTime = new Date();
+        // make player win that card
+        let winningPlayer = _.find(this.players, player => player.pID === winningCard.owner.pID);
+        // Add a copy of the ACard and QCard to the roundsWon of the winning player
+        winningPlayer.roundsWon.push({
+          roundNum: latestRound.roundNum,
+          ACard: winningCard,
+          QCard: latestRound.QCard
+        })
+        console.log(`${latestRound.winner} won with card ${latestRound.winningCard.text}`)
+      }
+      else {
+        console.log(`Attempted to play a winning card ${cardID} that was not played!`)
+      }
+    }
+    // set round.winningCard
     // set round.winner
     // set round.roundEndTime = new Date()
+  }
+
+  endRound() {
+    // TODO
+    // make a copy of the latestRound.cardsPlayed
+    // remove the 'owners' property on the cards
+    // place the cards back into the ACardDeck
+    // give all of the players (except for the judge), a new ACard from the deck
+    // 
+    // set round.active = false -- only do this when the first player selects "Tap anywhere to continue"
+    let latestRound = this.getLatestRound();
+    latestRound.active = false;
+  }
+
+  shuffleCard(sessionID, srcCardIDIndex, destCardIDIndex) {
+    // TODO
   }
 }
 
@@ -210,4 +250,13 @@ g.playCard('rezaSession#456', reza.cards[0].id);
 
 console.log(g.getPlayerRoundState('yusufSession#123'))
 console.log(g.getPlayerRoundState('salmanSession#456'))
+console.log(g.getPlayerRoundState('rezaSession#456'));
+
+// Yusuf choose winning card
+g.judgeSelectCard('yusufSession#123', g.getLatestRound().cardsPlayed[1].id);
 console.log(g.getPlayerRoundState('rezaSession#456'))
+// g.endRound();
+// console.log(g.getPlayerRoundState('rezaSession#456'))
+// g.endRound();
+// console.log(g.getPlayerRoundState('rezaSession#456'))
+
